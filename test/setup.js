60b205f3d8cbc46f565f3715129f05d17b18c98e
Sterling Watts <SterlingDWatts@gmail.com>
2020-03-11 11:37:37 -0700

    First commit

---
+++ test/setup.js
@@ -0,0 +1,13 @@
+process.env.TZ = "UTC";
+process.env.NODE_ENV = "test";
+process.env.JWT_SECRET = "03315099-0428-40a2-b1e9-59bf279ca3ea";
+process.env.JWT_EXPIRY = "5h";
+process.env.TEST_DATABSE_URL =
+  process.env.TEST_DATABASE_URL || "postgresql://example-test";
+
+require("dotenv").config();
+const { expect } = require("chai");
+const supertest = require("supertest");
+
+global.expect = expect;
+global.supertest = supertest;
